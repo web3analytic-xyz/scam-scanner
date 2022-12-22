537b4b4628a28f0b9662470956bb93ec8aa29999
mhw32 <me@mikewuis.me>
2022-12-22 11:51:45 -0800

    Fixes to the server for production

---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+
+const ApiContext = React.createContext({
+    response: {},
+    fetchResponse: () => {},
+})
+
+export default function QueryApi() {
+    const []
+}
