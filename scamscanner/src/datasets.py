754cd18a353f2f08813774658514d3252c98962b
mhw32 <me@mikewuis.me>
2022-12-21 20:43:14 -0800

    Add build vocab script

--- scamscanner/src/datasets.py
+++ scamscanner/src/datasets.py
@@ -1,6 +1,8 @@
+import json
 import h5py
 import torch
 import pandas as pd
+from collections import defaultdict
 
 from os.path import join
 from torch.utils.data import Dataset, DataLoader
@@ -9,26 +11,52 @@ from torch.nn.utils.rnn import pad_sequence
 from .paths import DATA_DIR
 
 
-def build_loaders(batch_size, num_workers=0):
-    r"""Create data loaders for training."""
+def build_bow_loaders(batch_size, num_workers=0):
+    r"""Create data loaders on Bag of Words datasets."""
 
-    train_dset = ContractDataset(split='train')
-    test_dset = ContractDataset(split='test')
+    train_dset = BagOfWordsDataset(split='train')
+    test_dset = BagOfWordsDataset(split='test')
 
-    train_loader = DataLoader(train_dset,
-                              batch_size=batch_size,
-                              shuffle=True, 
-                              pin_memory=True,
-                              num_workers=num_workers,
-                              collate_fn=collator,
-                              )
-    test_loader = DataLoader(test_dset,
-                             batch_size=batch_size,
-                             shuffle=False, 
-                             pin_memory=True,
-                             num_workers=num_workers,
-                             collate_fn=collator,
-                             )
+    train_loader = DataLoader(
+        train_dset,
+        batch_size=batch_size,
+        shuffle=True, 
+        pin_memory=True,
+        num_workers=num_workers,
+    )
+    test_loader = DataLoader(
+        test_dset,
+        batch_size=batch_size,
+        shuffle=False, 
+        pin_memory=True,
+        num_workers=num_workers,
+    )
+
+    return train_loader, test_loader
+
+
+def build_emb_loaders(batch_size, num_workers=0):
+    r"""Create data loaders on embedded datasets."""
+
+    train_dset = EmbeddedDataset(split='train')
+    test_dset = EmbeddedDataset(split='test')
+
+    train_loader = DataLoader(
+        train_dset,
+        batch_size=batch_size,
+        shuffle=True, 
+        pin_memory=True,
+        num_workers=num_workers,
+        collate_fn=collator,
+    )
+    test_loader = DataLoader(
+        test_dset,
+        batch_size=batch_size,
+        shuffle=False, 
+        pin_memory=True,
+        num_workers=num_workers,
+        collate_fn=collator,
+    )
 
     return train_loader, test_loader
 
@@ -52,7 +80,23 @@ def collator(list_of_dicts, padding_value=0):
     return batch_dict
 
 
-class ContractDataset(Dataset):
+class BagOfWordsDataset(Dataset):
+    r"""Bag-of-words dataset over op-codes."""
+
+    def __init__(self, split='train'):
+        super().__init__()
+
+        data = pd.read_csv(join(DATA_DIR, f'{split}.csv'))
+        data = data.reset_index(drop=True)
+
+        vocab_file = join(DATA_DIR, f'train-vocab.json')
+        with open(vocab_file, 'r') as fp:
+            vocab = json.load(fp)
+
+        self.data = data
+        self.vocab = vocab
+
+class EmbeddedDataset(Dataset):
     r"""Dataset of contracts with labels.
     Notes:
     --
