51564084ce18fb03a2709291ea472f733b4ec29f
mhw32 <me@mikewuis.me>
2022-12-22 16:55:55 -0800

    Fix

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className="footer-react col-12">
+            <footer>
+                Code is open-sourced with model checkpoints and training data downloadable on &nbsp;<a href="https://github.com/web3analytic-xyz/scam-scanner">github</a>. 
+                ScamScanner was built by the <a href="https://www.web3analytic.xyz">Web3Analytic</a> team. Follow us on <a href="https://twitter.com/Web3Analytic">twitter</a> for more product releases. 
+                Copyright © 2022.
+            </footer>
+        </div>
+    )
+}
---
+++ frontend/src/components/ResultsPage.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+export default function ResultsPage({
+    address,
+    prediction,
+    probability,
+    error,
+    initialized
+}) {
+    if (!initialized) {
+        return <div></div>;
+    }
+
+    var content;
+    if (error) {
+        content = (
+            <div className="results-content">
+                Something went wrong! Check that the address is for a smart contract (not an externally-owned account).
+            </div>
+        )
+    } else {
+        let etherscan = "https://etherscan.io/address/" + address;
+        probability = (probability * 100).toFixed(1);
+        var predictionText;
+        if (prediction) {
+            predictionText = <span style={{color: "rgb(243, 66, 66)"}}>YES</span>;
+        } else {
+            predictionText = <span style={{color: "rgb(0, 255, 0)"}}>NO</span>;
+        }
+        content = (
+            <div className="results-content">
+                <div>
+                    Processed smart contract at address <a href={etherscan}>{address}</a>. 
+                </div>
+                <div className="container">
+                    <div className="justify-center row">
+                        <div className="col-sm">
+                            <div className="size-container">
+                                <h3>Malicious?</h3>
+                                <div className="stat-container__value-container">
+                                    <div className="stat-container__value-wrap">
+                                        <div className="stat-container__value size">{predictionText}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                            <div className="size-container">
+                                <h3>Probability</h3>
+                                <div className="stat-container__value-container">
+                                    <div className="stat-container__value-wrap">
+                                        <div className="stat-container__value size">{probability}</div>
+                                    </div>
+                                    <div className="stat-container__unit">
+                                        %
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div className="results-page">
+            <div> 
+                <span className="logo-results">Scan Results</span>
+            </div>
+            {content}
+        </div>
+    )
+}
